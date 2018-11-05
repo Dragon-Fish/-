d1cedeb45b8eaede7a42036bb0ae265931214364
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2018-11-05 16:32:04 +0800

    Update ShowAvatar.js

--- extensions/Avatar/ShowAvatar.js
+++ extensions/Avatar/ShowAvatar.js
@@ -1,5 +1,13 @@
-$('#personal h2').prepend('<a href="/index.php/Special:uploadavatar"><img src="/extensions/Avatar/avatar.php?user=' + mw.config.get('wgUserName') + '" class="avatar"/></a>');
-$('.mw-userlink bdi,.ns-2 #firstHeading').prepend(function () {
-  var name = $(this).text();
-  $(this).prepend('<a href="/index.php/Special:uploadavatar"><img src="/extensions/Avatar/avatar.php?user=' + name+'" class="avatar"/></a>')
+$('#personal h2').prepend(function () {
+  var avatar = '<img src="/extensions/Avatar/avatar.php?user=' + mw.config.get('wgUserName') + '" class="avatar"/>';
+  if ($(window).width() > 851) {
+    $(this).prepend('<a href="/index.php/Special:uploadavatar">' + avatar + '</a>')
+  } else {
+    $(this).prepend(avatar)
+  }
 });
+$('.mw-userlink bdi').prepend(function () {
+  var name = $(this).text();
+  $(this).prepend('<img src="/extensions/Avatar/avatar.php?user=' + name + '" class="avatar"/>')
+});
+$('.ns-2 #firstHeading').prepend('<a href="/index.php/Special:uploadavatar"><img src="/extensions/Avatar/avatar.php?user=' + wgPageName + '" class="avatar"/></a>');
