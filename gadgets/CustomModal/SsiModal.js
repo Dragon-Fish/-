d46ed15d12b53985785f538c186b4313decbbf25
机智的小鱼君 <824399619@qq.com>
2020-10-05 10:43:33 +0800

    Restore gadgets folder

---
+++ gadgets/CustomModal/SsiModal.js
@@ -0,0 +1,2 @@
+mw.loader.load('https://cdn.bootcss.com/ssi-modal/1.0.27/js/ssi-modal.min.js');
+mw.loader.load('https://cdn.bootcss.com/ssi-modal/1.0.27/styles/ssi-modal.min.css','text/css');
---
+++ gadgets/Timeless-header-auto-hide/auto-hide-header.js
@@ -0,0 +1,19 @@
+/** 自动神隐顶部条 **/
+var lastScrollY = 0;
+window.onscroll = function() {
+    //变化量可自行修改，使其在指定高度内滚动时不隐藏
+    if (window.scrollY - lastScrollY > 0 && window.scrollY > 500)
+    {
+        //往下滚动，隐藏导航栏
+        //-70px修改为 -导航栏高度px
+        $('#mw-header-container,#mw-header-hack').addClass('header-hide');
+        $('.notify').addClass('notify-hide');
+    }
+    else if (window.scrollY - lastScrollY < 0)
+    {
+        //往上滚动，显示导航栏（Safari会有BUG，尴尬）
+        $('#mw-header-container,#mw-header-hack').removeClass('header-hide');
+        $('.notify').removeClass('notify-hide');
+    }
+        lastScrollY = window.scrollY;
+}
